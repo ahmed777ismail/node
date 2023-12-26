1b189926962aa7dc8bd712c2b2ae3676942a180e
ahmed777ismail <ahmed1kaballo@gmail.com>
2023-12-26 22:26:09 +0200

    lesson7

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
+app.use(express.urlencoded({ extended: true }));
+const Mydata = require("./models/mydataSchema");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
+//auto refresh
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
+
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+////////////////////////////////////////////////////////////////////////
 app.get("/", (req, res) => {
-  res.sendFile("./views/home.html", { root: __dirname });
+  res.render("index", {});
 });
-
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
+app.get("/user/add.html", (req, res) => {
+  res.render("user/add");
 });
+app.get("/user/view.html", (req, res) => {
+  res.render("user/view");
+});
+app.get("/user/edit.html", (req, res) => {
+  res.render("user/edit");
+});
+mongoose
+  .connect(
+    "mongodb+srv://devahmed:74tZ9crnUtpiDRrw@cluster0.hvoiv7c.mongodb.net/all-data?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
